ccfdb641e34207ff848a0eb9b3494de7c7e5ae90
Jan Luebbe <jluebbe@debian.org>
2023-02-05 22:43:48 +0100

    add qemu demo startup script

---
+++ qemu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE:-$0}")")"
+
+cd "${SCRIPT_DIR}/build/tmp-glibc/deploy/images/qemuarm/"
+
+qemu-system-arm -nographic -machine virt -cpu cortex-a15 \
+  -d unimp -m 1057 \
+  -kernel barebox-dt-2nd.img \
+  -device virtio-blk-device,drive=hd0 \
+  -drive if=none,file=qemu-virt-image.qcow,id=hd0
+
